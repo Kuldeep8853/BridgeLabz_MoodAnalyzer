f8626ccf6c7803134f62f10bf063f983b942b78c
Kuldeep Kasaudhan <59829843+Kuldeep8853@users.noreply.github.com>
2020-02-22 16:42:12 +0530

    [Kuldeep]Add.Throw null pointer exception

--- MoodAnalyserTest/MoodTest.cs
+++ MoodAnalyserTest/MoodTest.cs
@@ -12,33 +12,7 @@ namespace MoodAnalyserTest
     [TestFixture]
     public class Tests
     {
-        /// <summary>
-        /// test case 1.1 
-        /// test for sad mood
-        /// </summary>
-        [TestCase]
-        public void AnalyseSadMoodTest()
-        {
-            HappyOrSadMood mood = new HappyOrSadMood("I am in sad mood");
-            string expected = "Sad";
-            string actualResult = mood.AnalyseMood();
-            Assert.AreEqual(actualResult,expected);
-        }
-
-        /// <summary>
-        /// test case 1.2
-        /// test for any mood
-        /// </summary>
-        [TestCase]
-        public void AnalyseHappyMoodTest()
-        {
-            HappyOrSadMood mood = new HappyOrSadMood("I am in any mood");
-            string expected = "Happy";
-            string actualResult = mood.AnalyseMood();
-            Assert.AreEqual(actualResult, expected);
-        }
-
-        /// <summary>
+      /// <summary>
         /// test case 2.1
         /// test for any mood
         /// </summary>
